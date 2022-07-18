9fe85e8270075244ce94bc08d3f5f9c84470bec7
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2022-07-17 23:14:40 -0400

    Created /find

--- bot/commands/find.js
+++ bot/commands/find.js
@@ -4,8 +4,50 @@
  *  The VLC name, logo, and all other branding are property of the Virtual Learning Center.
  *--------------------------------------------------------------------------------------------*/
 
-const { discordClient, studentsCollection, guildsCollection, globals } = require('../../index');
+const {
+  discordClient,
+  studentsCollection
+} = require('../../index');
 
 module.exports = async function(interaction) {
-  globals.respond(interaction, true, '', 'Coming soon...');
+  const query = await studentsCollection.find({
+    $text: {
+      $search: interaction.options.data[0].value
+    }
+  }).sort({
+    score: {
+      $meta: "textScore"
+    }
+  })
+
+  let fields = [];
+  await query.forEach(async student => {
+    const user = await discordClient.users.fetch(student._id);
+
+    fields.push({
+      'name': student.name,
+      'value': `${user.username}#${user.discriminator} (<@${student._id}>)`,
+      'inline': false
+    });
+  });
+
+  const embed = {
+    'title': `Your search returned ${fields.length} result${fields.length != 1 ? "s" : ""}.`,
+    'fields': fields,
+    'footer': {
+      'iconURL': discordClient.user.displayAvatarURL(),
+      'text': 'VLC OneKey | Verified once, verified forever.'
+    },
+    'color': 5763719
+  };
+
+  if (!fields.length) { // no students found
+    embed.description = 'Please refine your search term.';
+    embed.color = 15548997;
+  };
+
+  await interaction.reply({
+    embeds: [embed],
+    ephemeral: true
+  });
 }
