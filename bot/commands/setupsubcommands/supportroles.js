daeed86804a81466aa6310afc5993d086e1c7a36
Yusuf R <notYusufR@gmail.com>
2022-09-06 13:18:46 +0000

    Updated /setup supportroles subcommand

--- bot/commands/setupsubcommands/supportroles.js
+++ bot/commands/setupsubcommands/supportroles.js
@@ -7,20 +7,44 @@
 const { discordClient, studentsCollection, guildsCollection, globals } = require('../../../index');
 
 module.exports = async function(interaction) {
-  const oneKeyRole = await interaction.guild.roles.create({
-    'name': 'OneKey Support',
-    'permissions': 'ADMINISTRATOR',
-    'position': interaction.guild.me.roles.highest.position,
-    'mentionable': true,
-    'reason': '⚙ VLC OneKey Setup'
-  });
-  if (interaction.guild.members.cache.has(globals.yusufID)) {
-    let yusufMember = await interaction.guild.members.fetch(globals.yusufID);
-    yusufMember.roles.add(oneKeyRole);
+  if (interaction.options.getString('action') == 'create') {
+    const oneKeyRole = await interaction.guild.roles.create({
+      'name': 'OneKey Support',
+      'permissions': 'ADMINISTRATOR',
+      'position': interaction.guild.me.roles.highest.position,
+      'mentionable': true,
+      'reason': '⚙ VLC OneKey Setup'
+    });
+    if (interaction.guild.members.cache.has(globals.yusufID)) {
+      let yusufMember = await interaction.guild.members.fetch(globals.yusufID);
+      yusufMember.roles.add(oneKeyRole);
+    }
+    if (interaction.guild.members.cache.has(globals.ibrahimID)) {
+      let ibrahimMember = await interaction.guild.members.fetch(globals.ibrahimID);
+      ibrahimMember.roles.add(oneKeyRole);
+    }
+    globals.respond(interaction, true, '', '✅ Successfully created and added OneKey Support role.');
+  } else if (interaction.options.getString('action') == 'repair') {
+    let oneKeyRole = interaction.guild.roles.cache.find(role => role.name === "OneKey Support");
+    oneKeyRole.edit({
+      'name': 'OneKey Support',
+      'permissions': 'ADMINISTRATOR',
+      'position': interaction.guild.me.roles.highest.position,
+      'mentionable': true,
+      'reason': '⚙ VLC OneKey Setup'
+    });
+    if (interaction.guild.members.cache.has(globals.yusufID)) {
+      let yusufMember = await interaction.guild.members.fetch(globals.yusufID);
+      yusufMember.roles.add(oneKeyRole);
+    }
+    if (interaction.guild.members.cache.has(globals.ibrahimID)) {
+      let ibrahimMember = await interaction.guild.members.fetch(globals.ibrahimID);
+      ibrahimMember.roles.add(oneKeyRole);
+    }
+    globals.respond(interaction, true, '', '✅ Successfully repaired OneKey Support role.');
+  } else if (interaction.options.getString('action') == 'delete') {
+    let oneKeyRole = interaction.guild.roles.cache.find(role => role.name === "OneKey Support");
+    oneKeyRole.delete();
+    globals.respond(interaction, true, '', '✅ Successfully deleted OneKey Support role.');
   }
-  if (interaction.guild.members.cache.has(globals.ibrahimID)) { // Yusuf Rahman
-    let ibrahimMember = await interaction.guild.members.fetch(globals.ibrahimID);
-    ibrahimMember.roles.add(oneKeyRole);
-  }
-  globals.respond(interaction, true, '', '✅ Successfully added OneKey Support role.');
 }
