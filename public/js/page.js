fa119470a8aed9cd70b25f2e1b80c397b0ca1c61
RedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2022-09-06 01:16:27 -0400

    Use GOOGLE_SECRET environment variable

--- public/js/page.js
+++ public/js/page.js
@@ -8,8 +8,7 @@ let googleUser = {};
 
 gapi.load("auth2", () => {
   auth2 = gapi.auth2.init({
-    client_id:
-      "357023733055-erbupt35eotgggefrj7hchlipd6ft8ao.apps.googleusercontent.com",
+    client_id: process.env["GOOGLE_SECRET"],
     cookiepolicy: "single_host_origin",
   });
 
