6339467a8b48c3cf670b62fa517d500c1bfa1e69
RedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2022-10-23 18:24:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -138,8 +138,7 @@ if (!DEVELOPMENT_MODE) {
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(require("./router"));
 
