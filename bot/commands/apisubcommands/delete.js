09055cef178c4da1bb641f8a2f2b4267958e9b8f
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2022-10-29 14:20:12 -0400

    update delete query to fetch student object

--- bot/commands/apisubcommands/delete.js
+++ bot/commands/apisubcommands/delete.js
@@ -7,19 +7,21 @@
 const { studentsCollection, keyCollection, globals } = require('../../../index');
 
 module.exports = async function(interaction) {
-  const request = await keyCollection.deleteOne({
-    student: {_id: interaction.options.data[0].options[0].user.id}
+  const student = await studentsCollection.findOne({
+    _id: interaction.options.data[0].options[0].user.id
   });
 
+  const request = await keyCollection.deleteOne({ student });
+
   if ((!request.deletedCount) && request.acknowledged) {
-      await globals.respond(interaction, false, '', `❌ ${interaction.options.data[0].options[0].user.tag} does not have an API key!`);
+    await globals.respond(interaction, false, '', `❌ ${interaction.options.data[0].options[0].user.tag} does not have an API key!`);
   } 
   
   else if (!request.acknowledged) {
-      await globals.respond(interaction, false, '', '❌ Failed to delete API key.');
+    await globals.respond(interaction, false, '', '❌ Failed to delete API key.');
   } 
 
   else {
-      await globals.respond(interaction, true, '', '✅ Successfully deleted API Key.');
+    await globals.respond(interaction, true, '', '✅ Successfully deleted API Key.');
   };
 }
