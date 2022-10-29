3658dead105aa29248f784683ba74632202ee30c
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2022-10-29 13:22:23 -0400

    Create /api delete /api create subcommands

--- bot/registerCommands.js
+++ bot/registerCommands.js
@@ -1,17 +1,43 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: 32727 });
 
-const commands = []
+const commands = [{
+    "name": "api",
+    "description": "Modify Developer API keys",
+    "options": [{
+            "type": 1,
+            "name": "create",
+            "description": "Create an API key",
+            "options": [{
+                "type": 6,
+                "name": "student",
+                "description": "Student must be verified with VLC OneKey.",
+                "required": true
+            }]
+        },
+        {
+            "type": 1,
+            "name": "delete",
+            "description": "Delete a student's API key",
+            "options": [{
+                "type": 3,
+                "name": "student",
+                "description": "Student must previously have an API key.",
+                "required": true
+            }]
+        }
+    ]
+}]
 
 client.on("ready", async () => {
-  console.log(`Logged in as ${client.user.tag}.`);
-  
-  for (let command of commands) {
-    const registeredCommand = await client.application.commands.create(command); // register the command
-    console.log(`✅ Created /${registeredCommand.name}`);
-  };
+    console.log(`Logged in as ${client.user.tag}.`);
 
-  client.destroy(); // end script
+    for (let command of commands) {
+        const registeredCommand = await client.application.commands.create(command); // register the command
+        console.log(`✅ Created /${registeredCommand.name}`);
+    };
+
+    client.destroy(); // end script
 });
 
 client.login(process.env["TOKEN"]);
